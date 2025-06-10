cd770e5e29b4634c3aa75a714ec006cdce37e661
batibatii <nbatikansevil@gmail.com>
2025-06-10 12:14:56 +0300

    Add responsive FAQ section with accordion functionality and new assets

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-const 
+const accordion = document.getElementsByClassName('opened__container');
+
+for (let i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener('click', function (e) {
+    this.classList.toggle('active');
+    e.stopPropagation();
+  })
+}
